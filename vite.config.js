b15692dfeb29904696b00d21b57181767a5d40ba
ouhammou rachid <93659459+ouhammmourachid@users.noreply.github.com>
2024-02-11 16:28:38 +0000

    Add react-refresh plugin to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 //vite.config.js
 import { resolve } from "path";
 import { defineConfig } from "vite";
-
-export default defineConfig ({
+import reactRefresh from "@vitejs/plugin-react-refresh";
+export default defineConfig({
+  plugins: [reactRefresh()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
       name: "morocco-svg",
-   		 
       fileName: "index",
     },
     rollupOptions: {
